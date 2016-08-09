fe63f794bbe1909bbc5725c1f5c11a5b03c16d98
Derya Aydede <derya.aydede@gmail.com>
2016-08-09 14:02:37 -0700

    moved line drawing to a function, updated world.json to have lat/long data for center of country and to have 3 and 5 char country codes under its objects/countries/geometries

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const GLOBE_RADIUS = 200;
+
 import { scene, camera, renderer } from './scene';
 import { setEvents } from './setEvents';
 import { convertToXYZ, getEventCenter, geodecoder, arc, get3DCoordsFrom2D } from './geoHelpers';
@@ -7,68 +9,20 @@ import topojson from 'topojson';
 import THREE from 'THREE';
 import d3 from 'd3';
 
-var GLOBE_RADIUS = 200;
 
-d3.json('data/world.json', function (err, data) {
+function map( x,  in_min,  in_max,  out_min,  out_max){return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;}
 
-  d3.select("#loading").transition().duration(500)
-    .style("opacity", 0).remove();
-
-  var currentCountry, overlay;
-
-  var segments = 155; // number of vertices. Higher = better mouse accuracy
-
-  // Setup cache for country textures
-  var countries = topojson.feature(data, data.objects.countries);
-  var geo = geodecoder(countries.features);
-
-  var textureCache = memoize(function (cntryID, color) {
-    var country = geo.find(cntryID);
-    return mapTexture(country, color);
-  });
-
-  // Base globe with blue "water"
-  let blueMaterial = new THREE.MeshPhongMaterial({color: '#003380', transparent: true});
-  let sphere = new THREE.SphereGeometry(GLOBE_RADIUS, segments, segments);
-  let baseGlobe = new THREE.Mesh(sphere, blueMaterial);
-  baseGlobe.rotation.y = Math.PI;
-  baseGlobe.addEventListener('click', onGlobeClick);
-  baseGlobe.addEventListener('mousemove', onGlobeMousemove);
-
-  // add base map layer with all countries
-  let worldTexture = mapTexture(countries, '#b8b894');
-  let mapMaterial  = new THREE.MeshPhongMaterial({map: worldTexture, transparent: true});
-  var baseMap = new THREE.Mesh(new THREE.SphereGeometry(200, segments, segments), mapMaterial);
-  baseMap.rotation.y = Math.PI;
-
-
-
-
-
-
-  // BEGIN HACKED IN LINE DRAWING TEST
-  // some helper function
-  function map( x,  in_min,  in_max,  out_min,  out_max){return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;}
-  // test json with 1 point
-  var dataRecord = {
-    from: { // in india
-      lat: 19,
-      lon: 78
-    },
-    to: { // in indonesia
-      lat: 0,
-      lon: 100
-    }
-  };
-  ///// calculations for getting FROM point from a latlong into a 3d point on the globe
-  var phiFrom = dataRecord.from.lat * Math.PI / 180;
-  var thetaFrom = (dataRecord.from.lon - 90) * Math.PI / 180;
+function getCurveObject(fLat, fLong, tLat, tLong)
+{
+  ////// calculating 3d points for FROM point
+  var phiFrom = fLat * Math.PI / 180;
+  var thetaFrom = (fLong - 90) * Math.PI / 180;
   var xF = GLOBE_RADIUS * Math.cos(phiFrom) * Math.sin(thetaFrom);
   var yF = GLOBE_RADIUS * Math.sin(phiFrom);
   var zF = GLOBE_RADIUS * Math.cos(phiFrom) * Math.cos(thetaFrom);
   ///// identical calculates for TO point
-  var phiTo = dataRecord.to.lat * Math.PI / 180;
-  var thetaTo = (dataRecord.to.lon - 90) * Math.PI / 180;
+  var phiTo = tLat * Math.PI / 180;
+  var thetaTo = (tLong - 90) * Math.PI / 180;
   var xT = GLOBE_RADIUS * Math.cos(phiTo) * Math.sin(thetaTo);
   var yT = GLOBE_RADIUS * Math.sin(phiTo);
   var zT = GLOBE_RADIUS * Math.cos(phiTo) * Math.cos(thetaTo);
@@ -102,34 +56,70 @@ d3.json('data/world.json', function (err, data) {
   // create curve geometry
   var geometry2 = new THREE.Geometry();
   geometry2.vertices = curve.getPoints( 50 );
-  var material2 = new THREE.LineBasicMaterial( { color : 'green' } );
-  
+  var material2 = new THREE.LineBasicMaterial( { color : 'orange' } );
+
   // CREATING ACTUAL 3D OBJECT TO RENDER:::
-  var curveObject = new THREE.Line( geometry2, material2 );
+  var blargh = new THREE.Line( geometry2, material2 );
+  return blargh;
   // added to scene a bit further below 
 
   // important: we need to save the paths for adding graphics to them::
   // paths.push(curve);
 
-  // make some cubes for testing, also added to scene below
-  var firstCube = new THREE.Mesh(new THREE.CubeGeometry(10,10,10), new THREE.MeshNormalMaterial());
-  firstCube.position.x = xT; firstCube.position.y = yT; firstCube.position.z = zT;
-  var secondCube = new THREE.Mesh(new THREE.CubeGeometry(10,10,10), new THREE.MeshNormalMaterial());
-  secondCube.position.x = xF; secondCube.position.y = yF; secondCube.position.z = zF;
-
   // END HACKED IN ROUTES TESTING
+}
 
 
 
+d3.json('data/world.json', function (err, data) {
+
+  d3.select("#loading").transition().duration(500)
+    .style("opacity", 0).remove();
+
+  var currentCountry, overlay;
+
+  var segments = 155; // number of vertices. Higher = better mouse accuracy
+
+  // Setup cache for country textures
+  var countries = topojson.feature(data, data.objects.countries);
+  var geo = geodecoder(countries.features);
+
+  var textureCache = memoize(function (cntryID, color) {
+    var country = geo.find(cntryID);
+    return mapTexture(country, color);
+  });
+
+  // Base globe with blue "water"
+  let blueMaterial = new THREE.MeshPhongMaterial({color: '#003380', transparent: true});
+  let sphere = new THREE.SphereGeometry(GLOBE_RADIUS, segments, segments);
+  let baseGlobe = new THREE.Mesh(sphere, blueMaterial);
+  baseGlobe.rotation.y = Math.PI;
+  baseGlobe.addEventListener('click', onGlobeClick);
+  baseGlobe.addEventListener('mousemove', onGlobeMousemove);
+
+  // add base map layer with all countries
+  let worldTexture = mapTexture(countries, '#b8b894');
+  let mapMaterial  = new THREE.MeshPhongMaterial({map: worldTexture, transparent: true});
+  var baseMap = new THREE.Mesh(new THREE.SphereGeometry(200, segments, segments), mapMaterial);
+  baseMap.rotation.y = Math.PI;
+
+  // get array of country objects from world.json
+  var countryArr = (data.objects.countries.geometries);
+  // create a container node and add all our curves to it
+  var curves = new THREE.Object3D();
+
+  countryArr.forEach(function(country){
+    curves.add(getCurveObject(country.lat, country.long, 0, 100));
+
+  });
 
 
-  // create a container node and add ALL OUR meshes
+  // create a container node and add all our meshes
   var root = new THREE.Object3D();
   root.scale.set(2.5, 2.5, 2.5);
   root.add(baseGlobe);
   root.add(baseMap);
-  root.add(curveObject);
-  root.add(firstCube); root.add(secondCube);
+  root.add(curves);
   scene.add(root);
 
   function onGlobeClick(event) {
