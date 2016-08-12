9c6dee268102c86657d2662948bef7263b66a280
Josh Han <xhan91ca@gmail.com>
2016-08-12 16:08:26 -0700

    single product search done

--- js/main.js
+++ js/main.js
@@ -210,3 +210,15 @@ function animate() {
 }
 
 animate();
+
+var panels = document.getElementsByClassName('panel');
+for(var i = 0; i < panels.length; i++){
+  panels[i].addEventListener('mouseenter', function(){
+    controls.enableZoom = false;
+    controls.enableRotate = false;
+  });
+  panels[i].addEventListener('mouseleave', function(){
+    controls.enableZoom = true;
+    controls.enableRotate = true;
+  });
+};
